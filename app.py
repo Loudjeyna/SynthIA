70a507223feb48a80bbedd88344a18c55cdeb062
Loudjeyna <loudjeynadjeddou@gmail.com>
2026-05-31 07:12:06 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly.express as px  # type: ignore
 import plotly.graph_objects as go
 from pathlib import Path
 import sys
